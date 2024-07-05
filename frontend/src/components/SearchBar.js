ccad18d9c44584892a3f3ad567329418ef2f618c
loheesong <loheesong@gmail.com>
2024-07-05 20:14:50 +0800

    feat: search bar display api results

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -2,22 +2,19 @@ import React, {useState} from 'react'
 import './SearchBar.css'
 import { FaSearch } from 'react-icons/fa'
 
+const autocomplete_api = "http://localhost:3001/api/autocomplete?q="
+
 /** Component: Search bar
  */
 function SearchBar({setResults}) {
     // take in setResults as a prop 
     const [searchIn, setSearchIn] = useState("");
+
+    // call api and set returned values to display in search results div 
     const fetchData = (value) => {
-        fetch("https://jsonplaceholder.typicode.com/users").then((res) => res.json()).then(json => {
-            const result = json.filter((user) => {
-                return (
-                    value && 
-                    user && 
-                    user.name && 
-                    user.name.toLowerCase().includes(value))
-            })
-            console.log(result);
-            setResults(result);
+        console.log("Querying: " + value);
+        fetch(autocomplete_api + value).then((res) => res.json()).then(json => {
+            setResults(json);
         })
     }
     const handleChange = (value) => {
--- frontend/src/components/SearchResults.js
+++ frontend/src/components/SearchResults.js
@@ -4,15 +4,16 @@ import './SearchResults.css'
 /** Search bar suggestions based on whats typed
  */
 function SearchResults({results}) {
-  return (
-    <div className="results-list">
-        {
-            results.map((result, id) => {
-                return <div key={id} className='search-result' onClick={(e) => alert(`Clicked on ${result.name}`)}>{result.name}</div>; // can be turned into a component 
-            })
-        }
-    </div>
-  )
+    return (
+        <div className="results-list">
+            {   
+                // must have unique id, put destination uid in key 
+                results.map((result, id) => {
+                    return <div key={id} className='search-result' onClick={(e) => alert(`Clicked on ${id}`)}>{result}</div>; // can be turned into a component 
+                })
+            }
+        </div>
+    )
 }
 
 export default SearchResults
