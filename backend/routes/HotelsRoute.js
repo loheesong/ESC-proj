9225c8280aea4efbd2050e271bba080de1f9ea86
xpxchxcx <118047971+xpxchxcx@users.noreply.github.com>
2024-07-09 15:02:14 +0800

    Fixed prices for hotels via destination id

--- backend/routes/HotelsRoute.js
+++ backend/routes/HotelsRoute.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const {getHotelDetailsFromHotelID , getHotelsFromDestinationID,
-  getHotelPricesByDestinationID,} = require("../controllers/hotelController");
+const {
+  getHotelDetailsFromHotelID,
+  getHotelsFromDestinationID,
+  getHotelPricesByDestinationID,
+} = require("../controllers/HotelController");
 
 // return hotels details based on hotelID
-// localhost:3001/hotels/050G
-router.get("/:id", getHotelDetailsFromHotelID);
+// localhost:3001/hotels/hotel/050G
+router.get("/hotel/:id", getHotelDetailsFromHotelID);
 
 // returns list of hotel details based on hotel destination id
-// localhost:3001/hotels?destination_id=WD0M
-router.get("/", getHotelsFromDestinationID);
+// localhost:3001/hotels/hotel?destination_id=WD0M
+router.get("/hotel", getHotelsFromDestinationID);
 
 // Returns list of possible hotel prices based on destination id, date of stay, guests and room.
-// localhost:3001/hotels/prices?destination_id=WD0M&checkin=2024-10-01&checkout=2024-10-08&country_code=SG&guests=2|2&partner_id=1
-router.get("/prices", getHotelPricesByDestinationID);
+// localhost:3001/hotels/filter?destination_id=WD0M&checkin=2024-10-01&checkout=2024-10-08&country_code=SG&guests=2|2&partner_id=1
+router.get("/filter", getHotelPricesByDestinationID);
 
-// Returns list of 
+// Returns list of
 
 module.exports = router;
